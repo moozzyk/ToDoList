6d5a153a4a8da4d176c3bcd747a74d23a86d041b
Pawel Kadluczka <moozzyk@gmail.com>
2015-09-14 14:10:18 -0700

    nits

--- src/ToDoList/Startup.cs
+++ src/ToDoList/Startup.cs
@@ -21,6 +21,7 @@ namespace ToDoList
                 configBuilder.AddUserSecrets();
             }
 
+
             configBuilder.AddEnvironmentVariables();
             Configuration = configBuilder.Build();
         }
